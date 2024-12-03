8bc81dc0b17fb1334a4a5d3e1bffeeebc25818bd
Saymon <silveirasaymonn@gmail.com>
2024-12-03 19:40:20 -0300

    clone2

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.get('/', (requisicao, resposta) => { 
     resposta.status(200).send(carros2024);       
 });
-    
+app.get('/ :sigla', (requisicao, resposta =>) {
+    }    
 
 // Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
